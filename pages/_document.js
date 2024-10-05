c9883639289155746a23317f7f3091668433b538
divrawat <divrawat2001@gmail>
2024-10-05 13:12:09 +0530

    sgv0er5g

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
     <Html lang="en">
       <Head >
         <link rel="icon" href={`${DOMAIN}/favicon.ico`} sizes="any" />
+        <meta name="msvalidate.01" content="A3B68AC93CC71F3C517A1D5F3CC7DDD1" />
       </Head>
       <body>
         <Main />
