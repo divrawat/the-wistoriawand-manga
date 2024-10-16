9602c2f743727f6246e8ef45959b39cc7c3c16b3
divrawat <divrawat2001@gmail>
2024-10-16 17:21:59 +0530

    sg0vdr5g

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,10 @@ export default function Document() {
       <Head >
         <link rel="icon" href={`${DOMAIN}/favicon.ico`} sizes="any" />
         <meta name="msvalidate.01" content="A3B68AC93CC71F3C517A1D5F3CC7DDD1" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script async crossOrigin="anonymous"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1721485376950080" />
+        <script async data-ad-client="ca-pub-1721485376950080" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" ></script>
       </Head>
       <body>
         <Main />
