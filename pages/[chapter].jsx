aadfe95cf59ccb8785dfd6f4cfaab7c0bfa7ac91
divrawat <divrawat2001@gmail>
2024-11-20 16:29:09 +0530

    sdegvs0dgevdsg5

--- pages/[chapter].jsx
+++ pages/[chapter].jsx
@@ -336,15 +336,15 @@ export default function Chapter({ chapterNumber, imageUrls, totalChapters, param
                 </ins>
 
 
-                <div className="bg-[black] relative">
-                    <div className="absolute inset-0 bg-black opacity-80"></div> {/* Dark overlay */}
+                {/* <div className="bg-[black] relative">
+                    <div className="absolute inset-0 bg-black opacity-80"></div>
                     <div className="pt-10 pb-10 max-w-[1100px] mx-auto px-5 text-[white] relative z-10">
                         <h2 className="text-center font-extrabold text-3xl">{`More About ${MANGA_NAME} Manga`}</h2>
                         {MANGA_SUMMARY.map(paragraph => (
                             <p className="py-7 leading-[2]" key={paragraph.id}>{paragraph.content}</p>
                         ))}
                     </div>
-                </div>
+                </div> */}
 
 
                 {/* <div className='py-10 bg-[#0f0511]'>
